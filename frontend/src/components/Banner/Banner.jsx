d30a03b27bce3f4e3d9c408e07636bfb611b9afc
Eyasu28 <eyasunigussie28@gmail.com>
2025-02-21 15:50:12 -0500

    Header component and Banner fully Implemented

---
+++ frontend/src/components/Banner/Banner.jsx
@@ -0,0 +1,37 @@
+import classes from "./Banner.module.css";
+
+const Banner = () => {
+    return (
+      <div
+        className={`d-flex text-center flex-column align-items-center justify-content-center ${classes.banner}`}
+      >
+        <div className={`pt-3 fw-light ${classes.banner__title}`}>
+          {/* <h1 className={` pt-3`}> */}
+          Location Solutions, <br />
+          Simplified.
+          {/* </h1> */}
+        </div>
+        <div className={`mt-3 fw ${classes.banner__description}`}>
+          Advanced location technology for businesses,
+          <br />
+          developers, and logistics providers
+        </div>
+        <div
+          className={`d-flex justify-content-around ${classes.banner__buttons}`}
+        >
+          <div
+            className={`px-4 py-3 rounded-3 fw-bold ${classes.banner__buttons__signUp}`}
+          >
+            Sign up for free
+          </div>
+          <div
+            className={`px-4 py-3 rounded-3 fs-6 ${classes.banner__buttons__contact}`}
+          >
+            Contact sales
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default Banner;
--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,50 +1,93 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./Header.module.css";
+import { BsChevronDown } from "react-icons/bs";
+import { IoPerson, IoPersonOutline } from "react-icons/io5";
 import logo from "../../assets/image/gebeta.app/GebetaMaps_-_Location_Solutions_Simplified/10026.svg";
+import person from "../../assets/image/gebeta.app/GebetaMaps_-_Location_Solutions_Simplified/10002.svg";
 
 const Header = () => {
+  const [isActive, setIsActive] = useState(false)
   return (
-    <div
-      className={`d-flex justify-content-between align-items-center  px-4 ${classes.header}`}
-    >
+    <>
       <div
-        className={`d-flex align-items-center gap-2 ${classes.header__logo}`}
+        className={`d-flex justify-content-between align-items-center px-4 py-3 ${classes.header}`}
       >
-        <div className={classes.logo__image}>
-          <img src={logo} alt="Logo" />
+        <div
+          className={`d-flex align-items-center gap-2 ${classes.header__logo}`}
+        >
+          <div className={classes.logo__image}>
+            <img src={logo} alt="Logo" />
+          </div>
+          <div className={`fw-bold ${classes.logo__text}`}>GebetaMaps</div>
         </div>
-        <div className={`fw-semibold ${classes.logo__text}`}>GebetaMaps</div>
-      </div>
-      <div className={`w-50 mb-0 ${classes.header__nav}`}>
-        <ul className={`d-flex mb-0 py-4 justify-content-evenly`}>
-          <li>
-            Products
-            {/* <select name="pricing" id="">
-              <option value="geocoding">Products</option>
-              <option value="geocoding">Geocoding</option>
-              <option value="route-optimization">Route Optimization</option>
-              <option value="direction">Direction</option>
-              <option value="matrix">Matrix</option>
-            </select> */}
-          </li>
-          <li>Company</li>
-          <li>Documentation</li>
-          <li>Pricing</li>
-        </ul>
-      </div>
-      <div
-        className={`d-flex align-items-center gap-3 ${classes.nav__buttons}`}
-      >
-        <div className={`fw-semibold ${classes.signIn__button}`}>Sign In</div>
-        <div className={`py-3 px-3 border-1 rounded-3 ${classes.getStarted__button}`}>
-          <button
-            className={`bg-white text-black border-0 fw-semibold `}
+        <div className={`${classes.header__nav}`}>
+          <ul className={`d-flex mb-0 justify-content-evenly`}>
+            <li
+              className={`${classes.orange__links} ${classes.products} mx-3 py-3`}
+              onMouseEnter={() => { setIsActive(true); }}
+              onMouseUp={()=>setIsActive(false)}
+            >
+              Products{" "}
+              <span>
+                <BsChevronDown />
+              </span>
+            </li>
+            <li className={`${classes.orange__links} mx-3 py-3`}>Company</li>
+            <li className={`${classes.orange__links} mx-3 py-3`}>
+              Documentation
+            </li>
+            <li className={`${classes.orange__links} mx-3 py-3`}>Pricing</li>
+          </ul>
+        </div>
+        <div
+          className={`d-flex align-items-center gap-3 ${classes.nav__buttons}`}
+        >
+          <div
+            className={`${classes.orange__links} fs-6 px-3 d-flex align-items-end ${classes.signIn__button}`}
+          >
+            <span className={`fs-5 mx-1 ${classes.person__icon}`}>
+              <img src={person} alt="" />
+            </span>{" "}
+            Sign In
+          </div>
+          <div
+            className={`py-3 px-4 rounded-3 fw-bold ${classes.getStarted__button} ${classes.orange__links}`}
           >
             Get Started
-          </button>
+          </div>
         </div>
       </div>
-    </div>
+      {isActive && (
+        <div className={`d-flex justify-content-evenly py-4 ${classes.hidden__Links}`}
+        onMouseLeave={()=>{setIsActive(false);}}>
+          <div className={`p-3 rounded-3`}>
+            <div className={`fw-bold mb-2`}>Geocoding</div>
+            <small className={`opacity-50 fw-light`}>
+              Convert addresses to coordinates and vice versa
+            </small>
+          </div>
+          <div className={`p-3 rounded-3`}>
+            <div className={`fw-bold mb-2`}>Route Optimization</div>
+            <small className={`opacity-50 fw-light`}>
+              Find the most efficient routes for multiple stops
+            </small>
+          </div>
+          <div className={`p-3 rounded-3`}>
+            <div className={`fw-bold mb-2`}>Directions</div>
+            <small className={`opacity-50 fw-light`}>
+              Get turn-by-turn navigation for various transportation modes
+            </small>
+          </div>
+          <div className={`p-3 rounded-3`}>
+            <div className={`fw-bold mb-2`}>Matrix</div>
+            <small className={`opacity-50 fw-light`}>
+              Calculate travel times and distances between multiple origins and
+              destinations
+            </small>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
